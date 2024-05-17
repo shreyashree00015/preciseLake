ffd7e6da83294bd0369d5bd5bc94844e3893b4a2
Shreya Shree S <92005528+shreyashree00015@users.noreply.github.com>
2024-05-17 14:24:42 +0530

    Update __init__.py

--- preciseLake/__init__.py
+++ preciseLake/__init__.py
@@ -1,1 +1,17 @@
+# __init__.py
 
+from .preciseLake import main, analyze_code, estimate_memory_usage, write_memory_usage_to_file, find_unused_code, find_non_terminating_functions, find_method_overrides, detect_redundant_calculations, detect_unused_imports, high_memory_components, code_to_graph
+
+__all__ = [
+    'main',
+    'analyze_code',
+    'estimate_memory_usage',
+    'write_memory_usage_to_file',
+    'find_unused_code',
+    'find_non_terminating_functions',
+    'find_method_overrides',
+    'detect_redundant_calculations',
+    'detect_unused_imports',
+    'high_memory_components',
+    'code_to_graph'
+]
